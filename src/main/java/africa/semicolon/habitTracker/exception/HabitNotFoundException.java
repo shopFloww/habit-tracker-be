c36e8b0c648fe7c7598ab0ebd67e77b911f99b18
WisdomAvan <codedplanet54113@gmail.com>
2026-09-12 13:29:29 +0100

    Fix habit not found exception inheritance

--- src/main/java/africa/semicolon/habitTracker/exception/HabitNotFoundException.java
+++ src/main/java/africa/semicolon/habitTracker/exception/HabitNotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class HabitNotFoundException extends HabitTracker {
+public class HabitNotFoundException extends RuntimeException {
     public HabitNotFoundException(Long habitId) {
         super("Habit with id " + habitId + " not found");
     }
