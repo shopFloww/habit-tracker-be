fef828d54d4d5abe7d2952245ad92f5b75e91deb
Your Name <youremail@example.com>
2026-09-13 14:34:19 +0100

    initial commit

--- src/main/java/africa/semicolon/habitTracker/service/HabitService.java
+++ src/main/java/africa/semicolon/habitTracker/service/HabitService.java
@@ -1,4 +1,18 @@
 package africa.semicolon.habitTracker.service;
 
+import africa.semicolon.habitTracker.model.Habit;
+
+import java.util.List;
+
 public interface HabitService {
+
+    Habit createHabit(Habit habit);
+
+    List<Habit> getAllHabits();
+
+    Habit getHabitById(Long id);
+
+    Habit updateHabit(Long id, Habit habit);
+
+    void deleteHabit(Long id);
 }
