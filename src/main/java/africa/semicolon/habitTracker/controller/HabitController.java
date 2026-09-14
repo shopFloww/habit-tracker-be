47f8590accbb4f06370e188597fb0c31e24b93c1
Amodu Enechojo Deborah <amoduchojo27@gmail.com>
2026-09-14 11:09:20 +0100

    Created user response and DTOs. 2. Created controller to Create user.

--- src/main/java/africa/semicolon/habitTracker/controller/HabitController.java
+++ src/main/java/africa/semicolon/habitTracker/controller/HabitController.java
@@ -1,4 +1,35 @@
 package africa.semicolon.habitTracker.controller;
 
+import africa.semicolon.habitTracker.dto.request.CreateUserRequest;
+import africa.semicolon.habitTracker.dto.response.userResponse;
+import africa.semicolon.habitTracker.service.HabitService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+
 public class HabitController {
+
+
+    private HabitService habitService;
+
+    @PostMapping
+    public ResponseEntity<userResponse> createUser(@RequestBody CreateUserRequest request){
+
+        userResponse response = new userResponse();
+        return  ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+    }
 }
+
+
+
+
+
---
+++ src/main/java/africa/semicolon/habitTracker/dto/response/userResponse.java
@@ -0,0 +1,10 @@
+package africa.semicolon.habitTracker.dto.response;
+
+public class userResponse {
+
+
+
+    private String username;
+    private String email;
+
+}
