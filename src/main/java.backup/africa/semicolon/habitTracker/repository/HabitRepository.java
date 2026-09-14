e92889adb180028fdbe888db612a8fb34dbe6893
djfemz <oladejifemi00@gmail.com>
2026-09-14 21:34:00 +0100

    chore: sanitized main branch

--- src/main/java.backup/africa/semicolon/habitTracker/repository/HabitRepository.java
+++
@@ -1,7 +0,0 @@
-package africa.semicolon.habitTracker.repository;
-
-import africa.semicolon.habitTracker.model.Habit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HabitRepository extends JpaRepository<Habit, Long> {
-}
