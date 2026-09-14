b076157c11a7501cd135400c30f191d6fb896965
akandeisaac021-design <akandeisaac021@gmail.com>
2026-09-14 16:54:34 +0100

    created habit entity

--- src/main/java/africa/semicolon/habitTracker/model/Habit.java
+++ src/main/java/africa/semicolon/habitTracker/model/Habit.java
@@ -1,5 +1,27 @@
 package africa.semicolon.habitTracker.model;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class Habit {
+
+    @Id
+    @ManyToOne
     private Long id;
+
+    private String name = "User-" + id;
+
+    private LocalDateTime created_at = LocalDateTime.now();
+    
+    private String description;
+
+    public void habit( String description ){
+        this.description = description;
+    }
 }
