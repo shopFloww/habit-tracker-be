3824ede382a55f6a30ed658f547a0bdaaeca9a03
BigGoodness <wsholanky@gmail.com>
2026-09-15 09:50:36 +0100

    test: add unit tests for HabitService

--- src/main/java/africa/semicolon/habitTracker/model/User.java
+++ src/main/java/africa/semicolon/habitTracker/model/User.java
@@ -1,34 +1,34 @@
-package africa.semicolon.habitTracker.model;
-
-
-import javax.annotation.processing.Generated;
-
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-@Entity
-
-public class User {
-    @Id
-    @GeneratedValue(strategy=GenerationType.UUID)
-    @Column(name="user_id")
-    private String userId;
-
-    @Column(name="userName")
-    @NotBlank
-
-    private String name;
-
-    @Column(name="email")
-    @NotBlank
-    @Email
-    private String email;
-
-
-
-
-
-
-
-}
+//package africa.semicolon.habitTracker.model;
+//
+//
+//import javax.annotation.processing.Generated;
+//
+//import jakarta.persistence.*;
+//import jakarta.validation.constraints.Email;
+//import jakarta.validation.constraints.NotBlank;
+//
+//@Entity
+//
+//public class User {
+//    @Id
+//    @GeneratedValue(strategy=GenerationType.UUID)
+//    @Column(name="user_id")
+//    private String userId;
+//
+//    @Column(name="userName")
+//    @NotBlank
+//
+//    private String name;
+//
+//    @Column(name="email")
+//    @NotBlank
+//    @Email
+//    private String email;
+//
+//
+//
+//
+//
+//
+//
+//}
