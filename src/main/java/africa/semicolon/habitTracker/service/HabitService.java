0646160cd9c015d95286d7dfd7cc37c0170ba83d
Femi <oladejifemi00@gmail.com>
2026-09-20 11:58:55 +0100

    Remove duplicate HabitService interface
    Removed redundant methods and imports from HabitService interface.

--- src/main/java/africa/semicolon/habitTracker/service/HabitService.java
+++ src/main/java/africa/semicolon/habitTracker/service/HabitService.java
@@ -4,28 +4,11 @@ import africa.semicolon.habitTracker.dtos.HabitRequestDto;
 import africa.semicolon.habitTracker.model.Habit;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface HabitService {
-
     List<Habit> findAllHabits(Long userId);
 
     Habit createHabit(HabitRequestDto habitRequestDto, Long userId);
 
     Habit findHabitById(Long id);
-import africa.semicolon.habitTracker.model.Habit;
-
-import java.util.List;
-
-public interface HabitService {
-
-    Habit createHabit(Habit habit);
-
-    List<Habit> getAllHabits();
-
-    Habit getHabitById(Long id);
-
-    Habit updateHabit(Long id, Habit habit);
-
-    void deleteHabit(Long id);
 }
